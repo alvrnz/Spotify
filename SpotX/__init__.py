f1bd36640bf38ca8659e1d9603f4fca02cd951b5
alvrnz <renz4you26@gmail.com>
2023-06-23 15:44:14 +0700

    From Haku

---
+++ SpotX/__init__.py
@@ -0,0 +1,37 @@
+from SpotX.core.bot import SpotXBot
+from SpotX.core.dir import dirr
+from SpotX.core.git import git
+from SpotX.core.userbot import Userbot
+from SpotX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = SpotXBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
